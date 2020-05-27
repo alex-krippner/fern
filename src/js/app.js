2c745905de3d68e3f85541c428e8d8d70c34f450
Alex Krippner <alexakrippner@gmail.com>
2020-05-27 14:36:54 +0200

    modifying carousel

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,17 @@
 //   btnCarouselOne = document.querySelector(".btn-carousel--1"),
 //   btnCarouselTwo = document.querySelector(".btn-carousel--2");
 
-import { elements } from "./views/base.js";
-import * as carouselView from "./views/carouselView.js";
+// import { elements } from "./views/base.js";
+// import * as carouselView from "./views/carouselView.js";
 /** Global state of the app
  * - Search object
  * - Current recipe object
  * - Shopping list object
  * - Liked recipes
  */
-const state = {
-  current: 0,
-};
-
-console.log(state.current);
+// const state = {
+//   current: 0,
+// };
 
 /**
  *
@@ -24,26 +22,26 @@ console.log(state.current);
 
 // button 1 click
 
-elements.btnCarouselOne.addEventListener("click", function () {
-  if (state.current === 0) {
-    state.current = elements.sliderImages.length;
-  }
+// elements.btnCarouselOne.addEventListener("click", function () {
+//   if (state.current === 0) {
+//     state.current = elements.sliderImages.length;
+//   }
 
-  carouselView.slideLeft(state.current);
-  state.current--;
-});
+//   carouselView.slideLeft(state.current);
+//   state.current--;
+// });
 
-// button 2 click
+// // button 2 click
 
-elements.btnCarouselTwo.addEventListener("click", function () {
-  if (state.current === elements.sliderImages.length - 1) {
-    state.current = -1;
-  }
-  carouselView.slideRight(state.current);
-  state.current++;
-});
+// elements.btnCarouselTwo.addEventListener("click", function () {
+//   if (state.current === elements.sliderImages.length - 1) {
+//     state.current = -1;
+//   }
+//   carouselView.slideRight(state.current);
+//   state.current++;
+// });
 
-carouselView.startSlide();
+// carouselView.startSlide();
 
 const nav = document.querySelector(".navigation");
 const linkNav = document.querySelectorAll(".link__navigation");
--- src/js/views/carouselView.js
+++ src/js/views/carouselView.js
@@ -1,30 +1,30 @@
-import { elements } from "./base.js";
+// import { elements } from "./base.js";
 
-// Clear all images
-export const resetCarousel = () => {
-  for (let i = 0; i < elements.sliderImages.length; i++) {
-    elements.sliderImages[i].style.display = "none";
-  }
-};
+// // Clear all images
+// export const resetCarousel = () => {
+//   for (let i = 0; i < elements.sliderImages.length; i++) {
+//     elements.sliderImages[i].style.display = "none";
+//   }
+// };
 
-// Init carousel
-export const startSlide = () => {
-  resetCarousel();
-  elements.sliderImages[0].style.cssText =
-    "display: block; background-size: cover";
-};
+// // Init carousel
+// export const startSlide = () => {
+//   resetCarousel();
+//   elements.sliderImages[0].style.cssText =
+//     "display: block; background-size: cover";
+// };
 
-// Show prev
-export const slideLeft = (current) => {
-  resetCarousel();
-  elements.sliderImages[current - 1].style.cssText =
-    "display: block; background-size: cover";
-};
+// // Show prev
+// export const slideLeft = (current) => {
+//   resetCarousel();
+//   elements.sliderImages[current - 1].style.cssText =
+//     "display: block; background-size: cover";
+// };
 
-// Show next
+// // Show next
 
-export const slideRight = (current) => {
-  resetCarousel();
-  elements.sliderImages[current + 1].style.cssText =
-    "display: block; background-size: cover";
-};
+// export const slideRight = (current) => {
+//   resetCarousel();
+//   elements.sliderImages[current + 1].style.cssText =
+//     "display: block; background-size: cover";
+// };
