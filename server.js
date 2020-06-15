58748207d2a7e6992c15a41fe9b9725c99e130d9
Alex Krippner <alexakrippner@gmail.com>
2020-06-15 10:47:49 +0200

    connected mongoAtlas with app through mongoose

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app.js');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successfull');
+  });
 
 const port = process.env.PORT || 8000;
 
