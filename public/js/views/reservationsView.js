6db800874c3b991386cde066f2d57a60fce6da0c
Alex Krippner <alexakrippner@gmail.com>
2020-07-25 12:13:28 +0200

    refactored reservationsView; separated local and exported functions

--- public/js/views/reservationsView.js
+++ public/js/views/reservationsView.js
@@ -20,22 +20,70 @@ let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
 let todayDate = today.getDate();
 let calendarDateClicked = false;
+let selectedDate;
+let templateMonth;
+let templateDay;
 
-export const renderCalendar = (month, year) => {
-  if (!month || !year) {
-    month = currentMonth;
-    year = currentYear;
+/*
+ ************************  LOCAL FUNCTIONS  ********************************
+ */
+
+const renderSummary = (name, party, month, day, email) => {
+  elements.resSummary.innerHTML = `
+  <p class="paragraph-primary">Hi ${name}, a table for ${
+    parseInt(party, 10) + 1
+  } has been booked for ${month} ${day}th.</p>
+  <p class="paragraph-primary">A booking confirmation has be sent to: ${email}</p>
+  <p class="paragraph-primary">We look forward to your visit.</p>
+
+  `;
+};
+
+// Check if calendar date has been selected
+
+const checkCalDates = () => {
+  if (!calendarDateClicked) {
+    document
+      .querySelector('.calendar__date-required')
+      .classList.add('calendar__date-required--reveal');
+  } else if (calendarDateClicked) {
+    document
+      .querySelector('.calendar__date-required')
+      .classList.remove('calendar__date-required--reveal');
   }
-  // Clear calendar
-  elements.dateGrid.textContent = '';
-  elements.monthIndicator.textContent = '';
+};
 
-  const firstDay = new Date(year, month).getDay();
-  const daysInMonth = 32 - new Date(year, month, 32).getDate();
+// Find selected calendar date
 
-  elements.monthIndicator.textContent = `${months[month]} ${year}`;
+const findSelectedCalDate = () => {
+  document.querySelectorAll('.calendar__dates').forEach((el) => {
+    if (
+      [...el.classList].find((index) => index === 'calendar__dates--selected')
+    ) {
+      selectedDate = el.childNodes[1].dateTime;
+      return selectedDate;
+    }
+  });
+};
 
-  // Create button element with child time element for each day of the month
+// Prepare data for rendering summary
+
+const prepSummaryRender = () => {
+  templateDay = selectedDate.split('-')[2];
+  templateMonth = months[parseInt(selectedDate.split('-')[1], 10) - 1];
+  // Hide reservations__body::after; reservations__form-details; reservations__calendar
+  const hideElArray = [elements.resFormDetails, elements.resCal];
+
+  hideElArray.forEach((el) => {
+    el.classList.add('reservations--hide');
+  });
+
+  elements.resBody.classList.add('reservations__body--hide');
+};
+
+// Render calendar dates function
+
+const renderCalendarDates = (daysInMonth, month, year) => {
   for (let i = 1; i <= daysInMonth; i++) {
     // Create button HTML tag for every day of the month and assign class and attribute
     const dayOfMonth = document.createElement('button');
@@ -56,19 +104,21 @@ export const renderCalendar = (month, year) => {
 
     elements.dateGrid.appendChild(dayOfMonth);
   }
+};
 
-  // Set the first day of the month to corresponding weekday in order correctly align weekdays and dates
-  const firstOfMonth = elements.dateGrid.firstChild;
-  firstOfMonth.setAttribute('style', `grid-column: ${firstDay + 1}`);
+// Disable 'calLeft' button
 
-  // Disable the left button when current month
-  if (currentMonth <= today.getMonth()) {
+const disableCalLeft = (curMonth) => {
+  if (curMonth <= today.getMonth()) {
     elements.btnCalLeft.disabled = true;
-  } else if (currentMonth > today.getMonth()) {
+  } else if (curMonth > today.getMonth()) {
     elements.btnCalLeft.disabled = false;
   }
+};
 
-  // Add event listener to 'calendar date' buttons whenever calendar is rendered
+// Setup event listener for calendar dates
+
+const setupCalDatesListener = () => {
   document.querySelectorAll('.calendar__dates').forEach((el) => {
     el.addEventListener('click', function () {
       // Remove 'selected date' styling from other button
@@ -89,7 +139,40 @@ export const renderCalendar = (month, year) => {
       return calendarDateClicked;
     });
   });
-  bookTableListener();
+};
+
+/*
+ ************************  EXPORTED FUNCTIONS  ********************************
+ */
+export const renderCalendar = (selectedMonth, selectedYear) => {
+  if (!selectedMonth || !selectedYear) {
+    selectedMonth = currentMonth;
+    selectedYear = currentYear;
+  }
+  const firstDay = new Date(selectedYear, selectedMonth).getDay();
+  const daysInMonth = 32 - new Date(selectedYear, selectedMonth, 32).getDate();
+
+  // Clear calendar
+  elements.dateGrid.textContent = '';
+  elements.monthIndicator.textContent = '';
+
+  // Update the calendar header with selected month and year
+  elements.monthIndicator.textContent = `${months[selectedMonth]} ${selectedYear}`;
+
+  // Create button element with child time element for each day of the month
+  renderCalendarDates(daysInMonth, selectedMonth, selectedYear);
+
+  // Set the first day of the month to corresponding weekday in order correctly align weekdays and dates
+
+  /*********** firstOfMonth MUST COME AFTER CALENDAR DATES RENDERED */
+  const firstOfMonth = elements.dateGrid.firstChild;
+  firstOfMonth.setAttribute('style', `grid-column: ${firstDay + 1}`);
+
+  // Disable the left button when current month
+  disableCalLeft(currentMonth);
+
+  // Add event listener to 'calendar date' buttons whenever calendar is rendered
+  setupCalDatesListener();
 };
 
 // CALENDAR BUTTON FUNCTIONALITY
@@ -104,21 +187,6 @@ export const previous = () => {
   currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
   currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
   renderCalendar(currentMonth, currentYear);
-};
-
-/*
- ************************  RENDER SUMMARY  ********************************
- */
-
-const renderSummary = (name, party, month, day, email) => {
-  elements.resSummary.innerHTML = `
-  <p class="paragraph-primary">Hi ${name}, a table for ${
-    parseInt(party, 10) + 1
-  } has been booked for ${month} ${day}th.</p>
-  <p class="paragraph-primary">A booking confirmation has be sent to: ${email}</p>
-  <p class="paragraph-primary">We look forward to your visit.</p>
-
-  `;
 };
 
 /*
@@ -150,20 +218,10 @@ export const validationListener = () => {
  */
 
 export const bookTableListener = () => {
-  let month;
-  let day;
   elements.btnReservations.addEventListener('click', (event) => {
-    let selectedDate;
+    // Check if calendar date selected
+    checkCalDates();
 
-    if (!calendarDateClicked) {
-      document
-        .querySelector('.calendar__date-required')
-        .classList.add('calendar__date-required--reveal');
-    } else if (calendarDateClicked) {
-      document
-        .querySelector('.calendar__date-required')
-        .classList.remove('calendar__date-required--reveal');
-    }
     if (
       document.getElementById('fname').checkValidity() &&
       document.getElementById('email').checkValidity() &&
@@ -171,52 +229,28 @@ export const bookTableListener = () => {
       calendarDateClicked
     ) {
       // Find selected calendar date
-      document.querySelectorAll('.calendar__dates').forEach((el) => {
-        if (
-          [...el.classList].find(
-            (index) => index === 'calendar__dates--selected'
-          )
-        ) {
-          selectedDate = el.childNodes[1].dateTime;
-          return selectedDate;
-        }
-      });
+      findSelectedCalDate();
 
       // Prepare day and month for use in template string
-      day = selectedDate.split('-')[2];
-      month = months[parseInt(selectedDate.split('-')[1], 10) - 1];
-      // Hide reservations__body::after; reservations__form-details; reservations__calendar
-      const hideElArray = [elements.resFormDetails, elements.resCal];
-
-      hideElArray.forEach((el) => {
-        el.classList.add('reservations--hide');
-      });
-
-      elements.resBody.classList.add('reservations__body--hide');
+      prepSummaryRender();
 
       // Render summary template
-
       renderSummary(
         document.getElementById('fname').value.split(' ')[0],
         document.getElementById('party').selectedIndex,
-        month,
-        day,
+        templateMonth,
+        templateDay,
         document.getElementById('email').value
       );
 
       // Delay the reveal of reservations__summary
       setTimeout(() => {
         elements.resSummary.classList.add('reservations__summary--reveal');
+
+        elements.btnReservations.setAttribute('style', 'visibility: hidden');
       }, 500);
       event.preventDefault();
     }
+    // event.preventDefault();
   });
 };
-
-// create modal covering reservations section
-// include reservations summary container
-// create template string with values of form controls
-
-// click on 'book table' button
-// set modal display to block
-//
